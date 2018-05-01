6300bf30867fd5d556f574b6e99de4f510b5093c
guzhaoyuan <guzhaoyuan@126.com>
2018-05-01 22:37:18 +0800

    added snakeHW abstract

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snake Robot
+
+a 7-DoF snake robot, using dynamixel MX-28AR, ROS Control.
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <snakeHW.h>
+#include <controller_manager/controller_manager.h>
+
+int main()
+{
+  MyRobot robot;
+  //controller_manager::ControllerManager cm(&robot);
+
+  while (true)
+  {
+     //robot.read();
+     //cm.update(robot.get_time(), robot.get_period());
+     //robot.write();
+     //sleep(5);
+  }
+  return 0;
+}
